606a8e9d7da73efbd7776481e8e228fe28fa71e4
Andrew Ray <andrew@svds.com>
2016-02-05 11:43:05 -0600

    formatting of setup.scala

--- 1-Reporting/setup.scala
+++ 1-Reporting/setup.scala
@@ -1,8 +1,11 @@
-val scaleFactor = 1
-println(s"*** Creating TPC-DS dataset at scale factor $scaleFactor")
 import com.databricks.spark.sql.perf.tpcds.Tables
+
+val scaleFactor = 1
+
+println(s"*** Creating TPC-DS dataset at scale factor $scaleFactor")
 val tables = new Tables(sqlContext, "/tpcds-kit/tools", 1)
 tables.genData("/tables", "parquet", true, false, false, false, false)
 tables.createExternalTables("/tables", "parquet", "default", false)
 println("*** Done generating tables")
+
 exit
